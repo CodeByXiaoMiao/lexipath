b4e34053e7b5a3ffec133941a526ac6181ba8ee6
Mybhsy <2644165755@qq.com>
2026-07-15 22:45:01 +0800

    feat: align vocabulary and IPA layouts

--- src/main.rs
+++ src/main.rs
@@ -91,8 +91,8 @@ fn main() -> eframe::Result<()> {
 
     let options = eframe::NativeOptions {
         viewport: eframe::egui::ViewportBuilder::default()
-            .with_inner_size([620.0, 520.0])
-            .with_min_inner_size([360.0, 220.0]),
+            .with_inner_size([900.0, 680.0])
+            .with_min_inner_size([640.0, 480.0]),
         ..Default::default()
     };
 
